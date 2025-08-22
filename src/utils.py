8c6e94cdf1a1086ad72eb2a5cd5c213b3276313b
dev079144 <dev079144@gmail.com>
2025-08-22 22:29:02 +0000

    Added data processing utils + BPE implementation

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,14 @@
+import re
 
+# Process raw data corpus
+def process_data(input_file = "./data/hemingway.txt", output_file = "./data/hemingway_processed.txt"):
+    with open(input_file, "r", encoding="utf-8") as f_in:
+        text = f_in.read()
+
+    text = re.sub(r'\s+', ' ', text)
+    text = text.strip()
+
+    with open(output_file, "w", encoding="utf-8") as f_out:
+        f_out.write(text)
+
+# BPE tokenizer
