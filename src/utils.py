15e953147d2ffb6a00e7f315ccae0fc1987328c9
dev079144 <dev079144@gmail.com>
2025-08-23 13:20:23 +0000

    BPE implementation

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,19 @@
 import re
+# import unicodedata
 
 # Process raw data corpus
 def process_data(input_file = "./data/hemingway.txt", output_file = "./data/hemingway_processed.txt"):
     with open(input_file, "r", encoding="utf-8") as f_in:
-        text = f_in.read()
+        data = f_in.read()
 
-    text = re.sub(r'\s+', ' ', text)
-    text = text.strip()
+    # ?
+    # data = unicodedata.normalize("NFKC", data)
+    # data = ''.join(c for c in data if not unicodedata.category(c).startswith('C'))
+
+    data = re.sub(r'\s+', ' ', data)
+    data = data.strip()
 
     with open(output_file, "w", encoding="utf-8") as f_out:
-        f_out.write(text)
+        f_out.write(data)
 
 # BPE tokenizer
