cff19e80f47d1e5b0a7746ded1dade1faca3a67d
dev079144 <dev079144@gmail.com>
2025-08-24 20:22:39 +0000

    Token ID to embedding vector conversion

--- src/config.py
+++ src/config.py
@@ -1,4 +1,8 @@
 # Hyperparameters
 
 # What's the optimal vocabulary size?
-vocabulary_size = 4000  # Tokens, post-BPE
+vocabulary_size = 4000  # Number of tokens in vocabulary post-BPE
+context_size = 8  # Number of tokens in a single training sequence
+batch_size = 32  # Number of training examples per batch
+embedding_dimension = 256 # Vector dimension for token embeddings
+scale_factor = 0.01  # TODO: Add description comment for this hyperparameter
